51f4f71e40516088c1a66e7059c260c577188f25
joao vitor de lima gonçalves <88281625+joaolima16@users.noreply.github.com>
2023-03-14 13:48:09 -0300

    Delete src directory

--- src/main/java/com/br/app/UserSystem/Models/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.app.UserSystem.Models.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.br.app.UserSystem.Models.Entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	
-}
